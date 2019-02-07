546da0290b1fe78025fa0b9c810ce3a35f12c9da
JeanerSilva <jeanerufmg@gmail.com>
2019-02-07 16:29:39 -0200

    First

---
+++ src/br/com/caelum/estoque/cliente/TesteServicoWWeb.java
@@ -0,0 +1,25 @@
+package br.com.caelum.estoque.cliente;
+
+public class TesteServicoWWeb {
+ public static void main(String[] args) {
+
+		EstoqueWS cliente = new EstoqueWS_Service().getEstoqueWSPort();
+
+		TokenUsuario tokenUsuario = new TokenUsuario();
+		
+		
+		//cliente.cadastrarItem(parameters, tokenUsuario);
+		
+		Filtros filtros = new Filtros();
+		Filtro filtro = new Filtro();
+		filtro.setNome("IPhone");
+		filtro.setTipo(TipoItem.CELULAR);
+		filtros.getFiltro().add(filtro);
+
+		ListaItens lista = cliente.todosOsItens(filtros);
+
+	 System.out.println("Lista: " +  lista.toString());
+} 
+	
+	
+}
